4c8b18135bb7e5af3dd2ed32219f19cba0f370d9
Gaetan Moreau <gmoreau.contact@gmail.com>
2023-03-23 15:27:49 +0100

    improve code

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 $words = array("chat", "chien", "oiseau", "poisson", "souris", "cheval", "vache", "cochon", "poulet", "lapin");
 
 $wordToGuess = str_split($words[array_rand($words)]);
-var_dump($wordToGuess);
 
-$victory = false;
 $lettersFound = [];
 $win = 5;
 
@@ -14,13 +12,10 @@ foreach ($wordToGuess as $lettre) {
     $tirets[] = "_";
 }
 
-print_r($tirets);
-
-while (!$victory) {
+while (true) {
     $saisie = readline("Donne une lettre : " . implode(' ', $tirets));
     if (in_array($saisie, $wordToGuess) && !in_array($saisie, $lettersFound)) {
-        array_push($lettersFound, $saisie);
-
+        $lettersFound[] = $saisie;
         foreach ($wordToGuess as $indice => $lettre) {
             if ($lettre == $saisie) {
                 $tirets[$indice] = $saisie;
@@ -29,17 +24,17 @@ while (!$victory) {
 
         echo "Tu as trouvé une lettre / ";
     } else {
-        if ($win !== 0) {
-            echo "Non essaye encore || ";
-            $win = $win - 1;
-        } else {
+        if (!$win) {
             echo "T'es mort";
-            exit();
+            exit;
         }
+
+        echo "Non essaye encore || ";
+        $win--;
     }
 
     if (count($lettersFound) === count($wordToGuess)) {
         echo "Bravo ma couille";
-        exit();
+        exit;
     }
 }
